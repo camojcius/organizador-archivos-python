723f29ec564e7c832a44b1b97cbff52b8c503b71
camojcius <ojciusc@gmail.com>
2026-04-26 16:01:29 -0300

    añado readme y modifico detalles

---
+++ README.md
@@ -0,0 +1,9 @@
+# Organizador de archivos de estudio 
+
+Script para ordenar documentos de materias de estudio, clasificándolos en "teoría", "práctica" o "indefinidos"
+
+
+## Cómo lo uso?
+1. Clona este repo
+2. Abrí la carpeta en la terminal y ejecutá `python organizador.py`
+3. Copiá la ruta de la carpeta a ordenar y pegala cuando el programa te lo pida
--- organizador.py
+++ organizador.py
@@ -1,8 +1,56 @@
-import pathlib
-import os
+from pathlib import Path 
 import shutil 
 
-path:str = input("Dirección de la carpeta a ordenar (ejemplo: documentos/carpeta1/cartpeta2/...)")
+textoPath = input("Dirección de la carpeta a ordenar (ejemplo: documentos/carpeta1/carpeta2/...)   ")
+pathCarpeta= Path(textoPath)
+
+
+while not pathCarpeta.exists():
+    print("\n La carpeta ingresada no se encuentra...\n")
+    textoPath = input("Dirección de la carpeta a ordenar (ejemplo: documentos/carpeta1/carpeta2/...)   ")
+    
+    pathCarpeta = Path(textoPath)
+
+nombreCarpeta= pathCarpeta.stem #guardo el nombre de la carpeta para crear sus subcarpetas
+
+textoSCPractica = "Práctica" + "_" + nombreCarpeta
+textoSCTeoria = "Teoría" + "_" + nombreCarpeta
+textoSCIndef = "Indefinido" + "_" + nombreCarpeta
+
+pathSCPractica = pathCarpeta / textoSCPractica
+pathSCTeoria = pathCarpeta / textoSCTeoria
+pathSCIndef = pathCarpeta / textoSCIndef
+
+pathSCPractica.mkdir(exist_ok=True)
+pathSCTeoria.mkdir(exist_ok=True)
+pathSCIndef.mkdir(exist_ok=True)
+
+palabrasPractica = ["práctica", "practica", "ejercicios", "guía", "guia", "guias", "guías","ejs"]
+palabrasTeoria = ["teoría", "teoria", "teórica", "teorica", "apunte", "apuntes", "libro", "diapositivas","diapositiva", "diapos", "clase"]
+
+for elemento in pathCarpeta.iterdir():
+
+    if elemento in [pathSCTeoria, pathSCPractica, pathSCIndef]:
+        continue
+
+
+    if elemento.is_dir():
+        shutil.move(elemento, pathSCIndef / elemento.name )#todas las carpetas van aca
+
+
+    elif elemento.is_file():
+        nombreFile= elemento.stem.lower()
+        
+        if any(palabra in nombreFile for palabra in palabrasPractica):
+            shutil.move(elemento, pathSCPractica) #guardo en carpeta práctica
+        
+        elif any(palabra in nombreFile for palabra in palabrasTeoria):
+            shutil.move(elemento, pathSCTeoria)
+        
+        else:
+            shutil.move(elemento, pathSCIndef)
+
+print("Carpeta de estudio ordenada!")
 
 
 
